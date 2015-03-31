01791d2196cb73a2d2517963d21391aabc8e6bc3
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-03-31 13:09:11 -0700

    allow user to submit posts

--- client/templates/requests/request_submit.js
+++ client/templates/requests/request_submit.js
@@ -1,3 +1,18 @@
 Template.requestSubmit.rendered = function() {
 	$('#date-picker').datepicker();
 }
+
+Template.requestSubmit.events({
+	'submit form': function(e) {
+		e.preventDefault();
+
+		var request = {
+			title: $(e.target).find('[name=title]').val(),
+			description: $(e.target).find('[name=description]').val(),
+			dueDate: $(e.target).find('[name=due-date]').val()
+		};
+
+		request._id = Requests.insert(request);
+		Router.go('requestPage', request);
+	}
+});
