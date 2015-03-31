e29684643ccd9b3aefc6b30262871c4beeb0f903
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-03-31 12:23:57 -0700

    move map into its own template

--- client/templates/requests/requests_list.js
+++ client/templates/requests/requests_list.js
@@ -1,26 +1,5 @@
 Template.requestsList.helpers({
   requests: function() {
     return Requests.find();
-  },
-  mapOptions: function() {
-    // Make sure the maps API has loaded
-    if (GoogleMaps.loaded()) {
-      // Map initialization options
-      return {
-        center: new google.maps.LatLng(37.7833, -122.4167),
-        zoom: 12
-      };
-    }
   }
 });
-
-Template.requestsList.onCreated(function() {
-// We can use the 'ready' callback to interact with the map API once the map is ready
-  GoogleMaps.ready('mapBanner', function(map) {
-    // Add a marker to the map once it's ready
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
-    });
-  });
-})
