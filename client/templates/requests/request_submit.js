7ed986d8fd6944b9810b5c34fd0d278f1c6f5c5c
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-03-31 18:11:56 -0700

    fix error styling on request submit template

--- client/templates/requests/request_submit.js
+++ client/templates/requests/request_submit.js
@@ -2,6 +2,19 @@ Template.requestSubmit.rendered = function() {
 	$('#date-picker').datepicker();
 }
 
+Template.requestSubmit.created = function() {
+	Session.set('requestSubmitErrors', {});
+}
+
+Template.requestSubmit.helpers({
+	errorMessage: function(field) {
+		return Session.get('requestSubmitErrors')[field];
+	},
+	errorClass: function(field) {
+		return !!Session.get('requestSubmitErrors')[field] ? 'has-error' : '';
+	}
+});
+
 Template.requestSubmit.events({
 	'submit form': function(e) {
 		e.preventDefault();
@@ -12,12 +25,17 @@ Template.requestSubmit.events({
 			dueDate: $(e.target).find('[name=due-date]').val()
 		};
 
+		var errors = validateRequest(request);
+		if (errors.title || errors.description || errors.dueDate) {
+			return Session.set('requestSubmitErrors', errors);
+		}
+
 		Meteor.call('requestInsert', request, function(error, result) {
 			// display the error to the user and abort
 			if (error) {
 				return throwError(error.reason);
 			}
-			
+
 			Router.go('requestPage', {_id: result._id});
 		});		
 	}
