5043b3315d3d1f78b645db143a69d0e8a6b387a1
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-04-05 15:59:28 -0700

    clean up

--- client/templates/includes/header.js
+++
@@ -1,9 +0,0 @@
-// Template.header.events({
-// 	'click .userProfile': function(e) {
-// 		e.preventDefault();
-
-// 		var user = Meteor.user();
-
-// 		Router.go('userProfile', {_id: user._id});
-// 	}
-// });
